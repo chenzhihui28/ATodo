ee1ddfaa29759975e05c8f59b22e07a66a3e4cc1
CHENZHIHUI193 <CHENZHIHUI193@pingan.com.cn>
2018-08-13 16:38:47 +0800

    first commit

---
+++ app/src/main/java/com/chenzhihuiiiii/atodo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.chenzhihuiiiii.atodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
